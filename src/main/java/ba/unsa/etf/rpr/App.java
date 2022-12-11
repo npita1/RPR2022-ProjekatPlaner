cfc66ae4722aba9760102817d4232ed801d532a1
npita1 <npita1@etf.unsa.ba>
2022-12-11 15:46:27 +0100

    Testirano delete

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -48,6 +48,8 @@ public class App
         System.out.println(user2.getGender());
         System.out.println(user2.getTokens());*/
 
+        /*// testiranje delete
+        dao.delete(12);*/
 
 
     }

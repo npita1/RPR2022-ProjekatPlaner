ffabdd7204e52d8f5ffb94fc24af977c80b27e99
npita1 <npita1@etf.unsa.ba>
2022-12-26 14:13:25 +0100

    Dodane maven dependencies za javafx i extendan App sa Application

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -3,57 +3,29 @@ package ba.unsa.etf.rpr;
 import ba.unsa.etf.rpr.dao.UserDao;
 import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.User;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 
-public class App
+public class App extends Application
 {
     public static void main( String[] args ) throws IOException {
         UserDao dao = new UserDaoSQLImpl();
         User user1 = new User("PrviClan","1234","M");
 
-        // add testiranje
+        //test baza
         dao.add(user1);
         System.out.println(user1.getId());
         System.out.println(user1.getUsername());
         System.out.println(user1.getPassword());
         System.out.println(user1.getGender());
         System.out.println(user1.getTokens());
+    }
 
-        /*// update testiranje
-        user1.setUsername("NoviName");
-        user1.setGender("F");
-        user1.setId(12);
-        dao.update(user1);
-        System.out.println(user1.getId());
-        System.out.println(user1.getUsername());
-        System.out.println(user1.getPassword());
-        System.out.println(user1.getGender());
-        System.out.println(user1.getTokens());*/
+    @Override
+    public void start(Stage stage) throws Exception {
 
-        /*// update 2 testiranje
-        User help = new User();
-        help.setUsername("NajnovijiUsername");
-        help.setPassword("najboljipass");
-        help.setGender(user1.getGender());
-        help.setId(user1.getId());
-        help.setTokens(user1.getTokens());
-        dao.update(help);*/
-
-        /*User user2;
-        user2 = dao.getById(12);
-        System.out.println(user2.getId());
-        System.out.println(user2.getUsername());
-        System.out.println(user2.getPassword());
-        System.out.println(user2.getGender());
-        System.out.println(user2.getTokens());*/
-
-        /*// testiranje delete
-        dao.delete(14);*/
-
-        dao.updateTokens(user1,50);
-        dao.updateTokens(user1,-20);
-        System.out.println(user1.getTokens());
     }
 }

11bd223a6475a2effcb33835986bb23735131b12
npita1 <npita1@etf.unsa.ba>
2022-12-29 22:55:16 +0100

    Dodano mijenjanje scena izmedju login i signup

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    private Stage stage;
+    private Scene scene;
+
+    public void switchToSignup(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/signup.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root,stage.getWidth(),stage.getHeight());
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
--- src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class SignupController {
+    private Stage stage;
+    private Scene scene;
+
+    public void switchToLogin(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root,stage.getWidth(),stage.getHeight());
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }

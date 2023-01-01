a0061a69583e6acebe34c4b740b7906917493a7c
npita1 <npita1@etf.unsa.ba>
2023-01-01 22:26:45 +0100

    Dodana funkcionalnost log outa koji vodi na login pocetnu stranu

--- src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
@@ -1,4 +1,27 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainController {
+
+    public MenuItem logOutButton;
+    private Stage stage;
+    private Scene scene;
+    public void switchToLogin(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        stage = (Stage)logOutButton.getParentPopup().getOwnerWindow();
+        //scene = stage.getScene();
+        //stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, stage.getScene().getWidth(),stage.getScene().getHeight());
+        stage.setScene(scene);
+        stage.show();
+    }
 }

a68eb26813bd8b5a2a4a82648d11e9c88055ca93
npita1 <npita1@etf.unsa.ba>
2023-01-07 22:25:54 +0100

    Napravljen prozor add subjects

--- src/main/java/ba/unsa/etf/rpr/controllers/SubjectTaskTabController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SubjectTaskTabController.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class SubjectTaskTabController {
+
+
+    public void openAddSubject (ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/fxml/addSubject.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 350, 450);
+        Stage stage = new Stage();
+        stage.setTitle("Add subject");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }

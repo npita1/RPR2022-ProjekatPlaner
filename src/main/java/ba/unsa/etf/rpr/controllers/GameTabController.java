815a4a478523758a11bc031ccc020f1ef582a90f
npita1 <npita1@etf.unsa.ba>
2023-02-19 20:06:00 +0100

    Dodano dodavanje teksta ako nema tokena

--- src/main/java/ba/unsa/etf/rpr/controllers/GameTabController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/GameTabController.java
@@ -3,10 +3,12 @@ package ba.unsa.etf.rpr.controllers;
 import ba.unsa.etf.rpr.business.UserManager;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.PlanerException;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 import java.text.ParseException;
 
@@ -37,6 +39,13 @@ public class GameTabController {
                 user.setTokens(user.getTokens() - 20);
                 userManager.update(user);
                 mainController.initialize();
+            } else {
+                if(!warningLabel.isVisible()) {
+                    warningLabel.setVisible(true);
+                    PauseTransition pause = new PauseTransition(Duration.seconds(3));
+                    pause.setOnFinished(e -> warningLabel.setVisible(false));
+                    pause.play();
+                }
             }
         }
 

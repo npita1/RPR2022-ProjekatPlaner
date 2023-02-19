e98fa203c6a7d546aca08b878db83a833f65c2a6
npita1 <npita1@etf.unsa.ba>
2023-02-19 18:26:49 +0100

    Dodan atribut usera u game tab kontroler radi mijenjanja broja tokena

--- src/main/java/ba/unsa/etf/rpr/controllers/GameTabController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/GameTabController.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.PlanerException;
 import javafx.fxml.FXML;
 
 public class GameTabController {
@@ -10,10 +12,12 @@ public class GameTabController {
 
     private MainController mainController;
 
-    @FXML
-    public void initialize() {
-        if(mainController != null) {
+    private User user;
 
+    @FXML
+    public void initialize() throws PlanerException {
+        if(mainController != null) {
+            user = userManager.getUserByUsername(mainController.getUsername());
         }
     }
 
@@ -21,6 +25,9 @@ public class GameTabController {
 
 
 
+
+
+    // Controller injections
     public void injectMainController(MainController mainController) {
         this.mainController = mainController;
     }

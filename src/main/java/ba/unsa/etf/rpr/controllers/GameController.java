7bed896e4c4509f7dbb2e0c22b45b77e700efc34
npita1 <npita1@etf.unsa.ba>
2023-02-20 18:39:08 +0100

    Dodane opcije za red igraca

--- src/main/java/ba/unsa/etf/rpr/controllers/GameController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/GameController.java
@@ -53,7 +53,7 @@ public class GameController {
         nowTurn = randomTurnArray.get(randomFirst);
         firstOneToPlay = nowTurn;
 
-        if(nowTurn.equals("AI")) {
+        if(firstOneToPlay.equals("AI")) {
             int randomButton =  ThreadLocalRandom.current().nextInt(0,enabledButtons.size());
             enabledButtons.get(randomButton).setText("X");
             enabledButtons.get(randomButton).setDisable(true);
@@ -81,6 +81,15 @@ public class GameController {
             button1.setDisable(true);
             enabledButtons.remove(button1);
         }
+        if(playerSign.equals("X")) {
+            button1.setText("X");
+            button1.setDisable(true);
+            enabledButtons.remove(button1);
+        } else {
+            button1.setText("O");
+            button1.setDisable(true);
+            enabledButtons.remove(button1);
+        }
     }
 
     public void button2Clicked(ActionEvent actionEvent) {
@@ -92,6 +101,15 @@ public class GameController {
             enabledButtons.remove(button2);
         } else {
             button2.setText("X");
+            button2.setDisable(true);
+            enabledButtons.remove(button2);
+        }
+        if(playerSign.equals("X")) {
+            button2.setText("X");
+            button2.setDisable(true);
+            enabledButtons.remove(button2);
+        } else {
+            button2.setText("O");
             button2.setDisable(true);
             enabledButtons.remove(button2);
         }
@@ -109,6 +127,15 @@ public class GameController {
             button3.setDisable(true);
             enabledButtons.remove(button3);
         }
+        if(playerSign.equals("X")) {
+            button3.setText("X");
+            button3.setDisable(true);
+            enabledButtons.remove(button3);
+        } else {
+            button3.setText("O");
+            button3.setDisable(true);
+            enabledButtons.remove(button3);
+        }
     }
 
     public void button4Clicked(ActionEvent actionEvent) {
@@ -123,11 +150,20 @@ public class GameController {
             button4.setDisable(true);
             enabledButtons.remove(button4);
         }
+        if(playerSign.equals("X")) {
+            button4.setText("X");
+            button4.setDisable(true);
+            enabledButtons.remove(button4);
+        } else {
+            button4.setText("O");
+            button4.setDisable(true);
+            enabledButtons.remove(button4);
+        }
     }
 
     public void button5Clicked(ActionEvent actionEvent) {
         // player O
-        if(nowTurn.equals("AI")) {
+        if(firstOneToPlay.equals("AI")) {
             nowTurn = "Player";
             button5.setText("O");
             button5.setDisable(true);
@@ -137,11 +173,20 @@ public class GameController {
             button5.setDisable(true);
             enabledButtons.remove(button5);
         }
+        if(playerSign.equals("X")) {
+            button5.setText("X");
+            button5.setDisable(true);
+            enabledButtons.remove(button5);
+        } else {
+            button5.setText("O");
+            button5.setDisable(true);
+            enabledButtons.remove(button5);
+        }
     }
 
     public void button6Clicked(ActionEvent actionEvent) {
         // player O
-        if(nowTurn.equals("AI")) {
+        if(firstOneToPlay.equals("AI")) {
             nowTurn = "Player";
             button6.setText("O");
             button6.setDisable(true);
@@ -151,11 +196,20 @@ public class GameController {
             button6.setDisable(true);
             enabledButtons.remove(button6);
         }
+        if(playerSign.equals("X")) {
+            button6.setText("X");
+            button6.setDisable(true);
+            enabledButtons.remove(button6);
+        } else {
+            button6.setText("O");
+            button6.setDisable(true);
+            enabledButtons.remove(button6);
+        }
     }
 
     public void button7Clicked(ActionEvent actionEvent) {
         // player O
-        if(nowTurn.equals("AI")) {
+        if(firstOneToPlay.equals("AI")) {
             nowTurn = "Player";
             button7.setText("O");
             button7.setDisable(true);
@@ -165,11 +219,20 @@ public class GameController {
             button7.setDisable(true);
             enabledButtons.remove(button7);
         }
+        if(playerSign.equals("X")) {
+            button7.setText("X");
+            button7.setDisable(true);
+            enabledButtons.remove(button7);
+        } else {
+            button7.setText("O");
+            button7.setDisable(true);
+            enabledButtons.remove(button7);
+        }
     }
 
     public void button8Clicked(ActionEvent actionEvent) {
         // player O
-        if(nowTurn.equals("AI")) {
+        if(firstOneToPlay.equals("AI")) {
             nowTurn = "Player";
             button8.setText("O");
             button8.setDisable(true);
@@ -179,11 +242,20 @@ public class GameController {
             button8.setDisable(true);
             enabledButtons.remove(button8);
         }
+        if(playerSign.equals("X")) {
+            button8.setText("X");
+            button8.setDisable(true);
+            enabledButtons.remove(button8);
+        } else {
+            button8.setText("O");
+            button8.setDisable(true);
+            enabledButtons.remove(button8);
+        }
     }
 
     public void button9Clicked(ActionEvent actionEvent) {
         // player O
-        if(nowTurn.equals("AI")) {
+        if(firstOneToPlay.equals("AI")) {
             nowTurn = "Player";
             button9.setText("O");
             button9.setDisable(true);
@@ -193,12 +265,21 @@ public class GameController {
             button9.setDisable(true);
             enabledButtons.remove(button9);
         }
+        if(playerSign.equals("X")) {
+            button9.setText("X");
+            button9.setDisable(true);
+            enabledButtons.remove(button9);
+        } else {
+            button9.setText("O");
+            button9.setDisable(true);
+            enabledButtons.remove(button9);
+        }
     }
 
     private void AITurn() {
         nowTurn = "AI";
         int randomButton = ThreadLocalRandom.current().nextInt(0,enabledButtons.size());
-        if(firstOneToPlay.equals("AI")) {
+        if(AISign.equals("X")) {
             enabledButtons.get(randomButton).setText("X");
             enabledButtons.get(randomButton).setDisable(true);
             enabledButtons.remove(enabledButtons.get(randomButton));
@@ -207,7 +288,6 @@ public class GameController {
             enabledButtons.get(randomButton).setDisable(true);
             enabledButtons.remove(enabledButtons.get(randomButton));
         }
-        nowTurn = "Player";
     }
 
 }

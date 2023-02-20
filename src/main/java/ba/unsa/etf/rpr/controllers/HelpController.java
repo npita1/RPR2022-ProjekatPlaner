849d802d47ba0a1d51cd99dc6d8e5e9f7d1c678a
npita1 <npita1@etf.unsa.ba>
2023-02-20 23:39:15 +0100

    Javadoc komentar 22

--- src/main/java/ba/unsa/etf/rpr/controllers/HelpController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HelpController.java
@@ -4,12 +4,17 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+/**
+ * Controller for managing help window
+ *
+ * @author Naida Pita
+ */
 public class HelpController {
-
     public Button okButton;
 
     public void closeWindow(ActionEvent actionEvent) {
         Stage stage = (Stage) okButton.getScene().getWindow();
         stage.close();
     }
+    
 }

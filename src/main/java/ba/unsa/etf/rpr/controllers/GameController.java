6623f3888ac8d39f63c0c0f2d2661076846a778f
npita1 <npita1@etf.unsa.ba>
2023-02-20 17:47:10 +0100

    Inicijalizacija buttona i gledamo ciji je red

--- src/main/java/ba/unsa/etf/rpr/controllers/GameController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/GameController.java
@@ -1,9 +1,54 @@
 package ba.unsa.etf.rpr.controllers;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class GameController {
 
-    
+
+    public GridPane buttonGrid;
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+    public Button button8;
+    public Button button9;
+
+
+    private ArrayList<Button> playerClicked;
+    private ArrayList<Button> AIClicked;
+    private final ArrayList<String> randomTurnArray = new ArrayList<String>(Arrays.asList("Player","AI"));
+    private String nowTurn;
+    private ArrayList<Button> enabledButtons;
+
+
+
+    @FXML
+    public void initialize() {
+        enabledButtons = new ArrayList<>(Arrays.asList(button1,button2,button3,button4,button5,button6,button7, button8,button9));
+    }
+
+
+    public void startGame(ActionEvent actionEvent) {
+
+        int randomFirst = ThreadLocalRandom.current().nextInt(0,1);
+        nowTurn = randomTurnArray.get(randomFirst);
+        if(nowTurn.equals("AI")) {
+
+        }
+
+    }
+
 
 }
 

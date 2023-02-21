86f89e1c73610a8f8d1a4cafc7a12f2901deb8ee
npita1 <npita1@etf.unsa.ba>
2023-02-21 01:27:11 +0100

    Javadoc komentar 47

--- src/main/java/ba/unsa/etf/rpr/dao/SubjectDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/SubjectDao.java
@@ -6,13 +6,31 @@ import ba.unsa.etf.rpr.exceptions.PlanerException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Dao interface for Subject domain bean
+ *
+ * @author Naida Pita
+ */
 public interface SubjectDao extends Dao<Subject>{
-    Subject getSubjectByName(String name) throws PlanerException;
-    Subject getSubjectByAcronym(String acronym) throws PlanerException;
-    Subject getSubjectByColor(String color) throws PlanerException;
 
+    /**
+     * Returns a subject with the given name
+     * @param name
+     * @return
+     * @throws PlanerException
+     */
+    Subject getSubjectByName(String name) throws PlanerException;
+
+    /**
+     * Returns a subject with the given acronym
+     * @param acronym
+     * @return
+     * @throws PlanerException
+     */
+    Subject getSubjectByAcronym(String acronym) throws PlanerException;
+
+    
     List<Subject> getSubjectsFromUserID(int id) throws PlanerException;
-    //List<Subject> getSubjectsFromUsername (String username) throws PlanerException;
 
     boolean duplicateSubjectForOneUser(int userID, String subjectName) throws PlanerException;
     boolean duplicateAcronymForUser(int userID,String acronym) throws PlanerException;

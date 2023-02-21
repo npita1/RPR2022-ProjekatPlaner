87c07f1a4b0452c3f47c83393c42805ddb24f549
npita1 <npita1@etf.unsa.ba>
2023-02-21 01:33:43 +0100

    Javadoc komentar 50

--- src/main/java/ba/unsa/etf/rpr/dao/TaskDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TaskDao.java
@@ -2,15 +2,33 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Task;
 import ba.unsa.etf.rpr.exceptions.PlanerException;
-import javafx.collections.ObservableList;
 
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Dao interface for Task domain bean
+ *
+ * @author Naida Pita
+ */
 public interface TaskDao extends Dao<Task>{
+
+    /**
+     * Returns all tasks for the given subject by id
+     * @param id
+     * @return
+     * @throws PlanerException
+     */
     ArrayList<Task> getTasksBySubjectID (int id) throws PlanerException;
 
+    /**
+     * 
+     * @param date
+     * @return
+     * @throws PlanerException
+     * @throws ParseException
+     */
     boolean checkDate(Date date) throws PlanerException, ParseException;
 
     boolean hasAnyTasks(int id) throws PlanerException;

863b572059314d7ab284cd5425c779c9a2955b06
npita1 <npita1@etf.unsa.ba>
2023-02-21 01:43:47 +0100

    Javadoc komentar 57

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -3,10 +3,19 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.PlanerException;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * Dao interface for To do list item domain bean
+ *
+ * @author Naida Pita
+ */
 public interface UserDao extends Dao<User>{
+
+    /**
+     * 
+     * @param username
+     * @return
+     * @throws PlanerException
+     */
     User getByUsername (String username) throws PlanerException;
     User updateTokens(User item, Integer value);
 

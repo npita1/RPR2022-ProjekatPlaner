4414a34113bc0b38213e29cd10534cc1bce5d7dc
npita1 <npita1@etf.unsa.ba>
2023-02-21 01:45:03 +0100

    Javadoc komentar 58

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -11,18 +11,44 @@ import ba.unsa.etf.rpr.exceptions.PlanerException;
 public interface UserDao extends Dao<User>{
 
     /**
-     * 
+     * Returns user from username
      * @param username
      * @return
      * @throws PlanerException
      */
     User getByUsername (String username) throws PlanerException;
+
+    /**
+     * Updates tokens
+     * @param item
+     * @param value
+     * @return
+     */
     User updateTokens(User item, Integer value);
 
+    /**
+     * Checks if the username is valid
+     * @param username
+     * @return
+     * @throws PlanerException
+     */
     boolean validUsername(String username) throws PlanerException;
 
+    /**
+     * Checks if the password is valid
+     * @param username
+     * @param password
+     * @return
+     * @throws PlanerException
+     */
     boolean validPassword(String username, String password) throws PlanerException;
 
+    /**
+     * 
+     * @param username
+     * @return
+     * @throws PlanerException
+     */
     boolean validNewUsernameExist(String username) throws PlanerException;
 
     boolean validNewUsernameLength (String username) throws PlanerException;

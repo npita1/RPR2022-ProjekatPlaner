15bf6372b8aa41b23df70e13f40cbf10104ae91c
npita1 <npita1@etf.unsa.ba>
2023-02-21 01:38:34 +0100

    Javadoc komentar 52

--- src/main/java/ba/unsa/etf/rpr/dao/ToDoListDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ToDoListDao.java
@@ -5,10 +5,27 @@ import ba.unsa.etf.rpr.exceptions.PlanerException;
 
 import java.util.ArrayList;
 
+/**
+ * Dao interface for To do list item domain bean
+ *
+ * @author Naida Pita
+ */
 public interface ToDoListDao extends Dao<ToDoList> {
 
+    /**
+     * Returns To do list item that is connected to the given task id
+     * @param id
+     * @return
+     * @throws PlanerException
+     */
     ToDoList getToDoListItemByTaskID (int id) throws PlanerException;
 
+    /**
+     * 
+     * @param id
+     * @return
+     * @throws PlanerException
+     */
     ArrayList<ToDoList> getAllTODOTasksFromUser (int id) throws PlanerException;
     ArrayList<ToDoList> checkIfTaskIDExists(int taskId) throws PlanerException;
 
